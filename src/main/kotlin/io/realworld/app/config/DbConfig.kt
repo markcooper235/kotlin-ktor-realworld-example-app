75f27c4dfa88a512adf3dbac7cc45b3ec55648e6
markcooper235 <mark.cooper235@gmail.com>
2026-06-29 11:47:41 -0400

    Enforce coverage and stabilize baseline tests

--- src/main/kotlin/io/realworld/app/config/DbConfig.kt
+++ src/main/kotlin/io/realworld/app/config/DbConfig.kt
@@ -2,8 +2,16 @@ package io.realworld.app.config
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.realworld.app.domain.repository.ArticleTags
+import io.realworld.app.domain.repository.Articles
+import io.realworld.app.domain.repository.Favorites
+import io.realworld.app.domain.repository.Follows
+import io.realworld.app.domain.repository.Tags
+import io.realworld.app.domain.repository.Users
 import org.h2.tools.Server
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 object DbConfig {
     fun setup(jdbcUrl: String, username: String, password: String) {
@@ -14,5 +22,8 @@ object DbConfig {
             config.password = password
         }
         Database.connect(HikariDataSource(config))
+        transaction {
+            SchemaUtils.create(Users, Follows, Tags, Articles, ArticleTags, Favorites)
+        }
     }
 }
